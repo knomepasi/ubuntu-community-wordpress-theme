fc706813fb38dd0210d0fb94501814c9af50694c
Pasi Lallinaho <pasi@shimmerproject.org>
2016-01-11 01:24:43 +0200

    Cleanup.

--- content.php
+++ content.php
@@ -1,42 +1,35 @@
-<?php if( have_posts( ) ) { ?>
-	<?php while( have_posts( ) ) { ?>
-		<?php the_post( ); ?>
+<?php while( have_posts( ) ) { ?>
+	<?php the_post( ); ?>
 
-		<div <?php post_class( 'group' ) ?>>
-			<?php if( !is_front_page( ) ) { ?>
-				<h1 class="post-title"><?php the_title(); ?></h1>
-			<?php } ?>
+	<div <?php post_class( 'group' ) ?>>
+		<?php if( !is_front_page( ) ) { ?>
+			<h1 class="post-title"><?php the_title(); ?></h1>
+		<?php } ?>
 
-			<?php /* post itself */ ?>
-			<div class="post-post">
-				<div class="post-entry entry"><?php the_content( __( 'Read the rest of this article &raquo;', 'ubuntu-teams' ) ); ?></div>
-			</div>
-
-			<?php /* post meta data */ ?>
-			<?php if( !is_page( ) ) { ?>
-				<div class="post-meta">
-					<span class="post-date">
-						<?php echo date_i18n( get_option( 'date_format' ), get_the_time( 'U' ) ); ?>
-					</span>
-					<?php if( !is_single() && !is_page() ) { ?>
-						<span class="comments">
-							&mdash; <?php comments_popup_link( __( 'No comments &raquo;', 'ubuntu-teams' ), __( '1 comment &raquo;', 'ubuntu-teams' ), sprintf( __( '%1$s comments &raquo;', 'ubuntu-teams' ), "%" ) ); ?>
-						</span>
-					<?php } ?>
-					<br />
-					<span class="post-cat"><?php the_category( ', ' ); ?></span>
-					<?php the_tags( '&mdash; <span class="post-tags">', ', ', '</span>' ); ?>
-				</div>
-			<?php } ?>
+		<?php /* post itself */ ?>
+		<div class="post-post">
+			<div class="post-entry entry"><?php the_content( __( 'Read the rest of this article &raquo;', 'ubuntu-teams' ) ); ?></div>
 		</div>
 
+		<?php /* post meta data */ ?>
 		<?php if( !is_page( ) ) { ?>
-			<?php comments_template( ); ?>
+			<div class="post-meta">
+				<span class="post-date">
+					<?php echo date_i18n( get_option( 'date_format' ), get_the_time( 'U' ) ); ?>
+				</span>
+				<?php if( !is_single() && !is_page() ) { ?>
+					<span class="comments">
+						&mdash; <?php comments_popup_link( __( 'No comments &raquo;', 'ubuntu-teams' ), __( '1 comment &raquo;', 'ubuntu-teams' ), sprintf( __( '%1$s comments &raquo;', 'ubuntu-teams' ), "%" ) ); ?>
+					</span>
+				<?php } ?>
+				<br />
+				<span class="post-cat"><?php the_category( ', ' ); ?></span>
+				<?php the_tags( '&mdash; <span class="post-tags">', ', ', '</span>' ); ?>
+			</div>
 		<?php } ?>
-	<?php } ?>
-<?php } else { ?>
-	<div class="notfound">
-		<h3><?php _e( 'Page not found.', 'ubuntu-teams' ); ?></h3>
-		<p><?php _e( 'The page you were looking for was not found.', 'ubuntu-teams' ); ?></p>
 	</div>
+
+	<?php if( !is_page( ) ) { ?>
+		<?php comments_template( ); ?>
+	<?php } ?>
 <?php } ?>
--- index.php
+++ index.php
@@ -12,7 +12,16 @@
 	</div>
 
 	<div id="main" class="inside"><div class="pad">
-		<?php get_template_part( 'content', get_post_format() ); ?>
+		<?php
+			if( have_posts( ) ) {
+				get_template_part( 'content', get_post_format( ) );
+			} else {
+				?><div class="notfound">
+					<h3><?php _e( 'Page not found.', 'ubuntu-community' ); ?></h3>
+					<p><?php _e( 'The page you were looking for was not found.', 'ubuntu-community' ); ?></p>
+				</div><?php
+			}
+		?>
 	</div></div></div>
 
 <?php get_footer( ); ?>
